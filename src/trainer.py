b6da4720f889f8962383f0c98cff5f135ffda36a
Oh-Donggyu <dgn6954@gmail.com>
2021-12-01 10:14:52 +0000

    fix: conflict log, classification report bug fix

--- src/trainer.py
+++ src/trainer.py
@@ -201,12 +201,8 @@ class TorchTrainer:
             best_test_f1 = test_f1
             self.stopping_count = 0
             print(f"Model saved. Current best test f1: {best_test_f1:.3f}")
-<<<<<<< HEAD
-<<<<<<< HEAD
-            if best_test_f1 > 0.6:
-=======
+
             if best_test_f1 > 0.5:
->>>>>>> 553d104cf3637523fbcb1ee9b8810d0683798b99
                 save_model(
                     model=self.model,
                     path=os.path.join(self.log_dir, "best.pt"),
@@ -214,15 +210,6 @@ class TorchTrainer:
                     device=self.device,
                 )
                 save_classification_report(path=self.log_dir, preds=preds, gt=gt)
-=======
-            save_model(
-                model=self.model,
-                path=os.path.join(self.log_dir, "best.pt"),
-                data=data,
-                device=self.device,
-            )
-            save_classification_report(path=self.log_dir, preds=preds, gt=gt)
->>>>>>> origin
 
         return best_test_acc, best_test_f1
 
